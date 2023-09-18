3b3e08232102efc8bb68088d560dbf4eba884df6
horThana <thanawat.maphet@gmail.com>
2023-09-18 21:13:51 +0700

    install daisyui

--- src/Activity.jsx
+++ src/Activity.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Layout from './Layout.jsx'
 import Form from './Form'
 import ActivityCard from './components/Activitycard.jsx'
+import Exercise_List from './Exercise_List.jsx'
+
 
 const Activity = () => {
 
     return (
         <Layout>
             <h1>Activity Page</h1>
-            <Form/>
+            {/* <Exercise_List/> */}
+            {/* <Form/> */}
+            <ActivityCard/>
             
         </Layout>
     )
