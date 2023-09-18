f03a68a324ee3ebaa3ac42d3faa4513488128c8f
ninimininomnom <warunya694@gmail.com>
2023-09-18 19:37:01 +0700

    code and fixed form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import React from 'react'
+import Form from './Form'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers';
 
 
 function App() {
 
   return (
-  <>
-    
-  </>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Form/>
+      </LocalizationProvider>
   )
     
   }
