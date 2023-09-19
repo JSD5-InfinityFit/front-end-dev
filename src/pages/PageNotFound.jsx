ee7d0b1c8050fbb830a752e9914e0e4d23779036
Ruchida Pithaksiripan <im@fai.in.th>
2023-09-19 10:37:44 +0700

    fix bug in ActivityValidation

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Layout from '../Layout.jsx'
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404 Error</h1>
-      <h1>Page Not Found</h1>
-    </div>
+    <Layout>
+      <section class="page_404">
+	    <div class="container">
+		<div class="row">	
+		<div class="col-sm-12 ">
+		<div class="col-sm-10 col-sm-offset-1  text-center">
+		  <div class="four_zero_four_bg">
+		    <h1 class="text-center ">404</h1>
+		  </div>
+		  <div class="contant_box_404">
+		  <h3 class="h2">
+		  Look like you're lost
+		</h3>
+		
+		<p>the page you are looking for not avaible!</p>
+		
+		<a href="/" class="link_404">Go to Home</a>
+	</div>
+		</div>
+		</div>
+		</div>
+	  </div>
+      </section>
+    </Layout>
   )
 }
 
