b567532d47c2bd8dc171ca6ab9d0a544ada7b1ab
ChutimanKrataithong <chutiman.krataithong@gmail.com>
2023-10-18 10:55:30 +0700

    Edit Css font weight, font size,position text-box,media quary

--- src/components/ActivityList.jsx
+++ src/components/ActivityList.jsx
@@ -1,29 +1,4 @@
-// import { useEffect, useState } from 'react'
-// import './ActivityList.css'
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-
-// function ActivityList() {
-//   const [cards,setCards ] = useState([]);
-  
-//   useEffect(() => {
-//     getDataFromAPI();
-//   }, []);
-
-//   const getDataFromAPI = async () => {
-//     await axios.get('https://infinity-fit-backend.onrender.com/activities')
-//     .catch((err) => {
-//       console.log('Error', err)
-//     })
-//     .then((res) => {
-//       console.log('Success', res.data)
-//       setCards(res.data);
-//       console.log(cards);
-//     })
-//   };
-
 import { useEffect, useState } from 'react';
-// import './ActivityList.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -51,20 +26,15 @@ function ActivityList() {
 
   const formatApiDate = (apiDate) => {
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const monthsOfYear = ['Jan','Feb','Mar','May','Jun','Jul','Aug','Sep','Oct','Novr','Dec'];
+    const monthsOfYear = ['Jan','Feb','Mar','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
     const date = new Date(apiDate);
     const day = date.getDate().toString().padStart(2, '0');
-    // const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-based
-    // const month = monthsOfYear[(date.getMonth()-1).toString().padStart(2, '0')]
     const month = monthsOfYear[date.getMonth()-1]
 
-    // const year = date.getFullYear().toString().slice(-2);
     const dayOfWeek = daysOfWeek[date.getDay()];
-    // const monthOfyear = monthsOfyear[date.month.getMonth()];
-
-    // return `${day} ${month} ${year}`;
-    // return `${dayOfWeek}${day}${month} `;
+    
+    
     return {
       dayOfWeek,day,month,
     };
@@ -74,30 +44,30 @@ function ActivityList() {
   return (
     <>
         <section className='box-border bg-[#F0F8FF]'>
-          <div className='container w-full max-w-screen-xl m-auto '>
-            <h1 className='text-5xl pt-6'>Exercise List</h1>
-              <div className='flex grid flex-wrap justify-between grid-cols-4 gap-3 mt-3 cards '>
+          <div className='sm:container w-full max-w-screen-xl m-auto '>
+            <h1 className='text-5xl pt-6 font-semibold	'>Exercise List</h1>
+
+              <div className='grid flex-wrap justify-between grid-rows-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-3 cards container md:mx-auto'>
                 {cards.map((card) =>(
                   <Link to={`/activities/${card._id}`} key={card._id} >
                     <div 
                       className='flex flex-row flex-shrink-0 w-full p-3 card bg-blue-950 flex-0 flex-basis-calc max-w-calc'>
                       
-                      {/* <div className='text-4xl font-black text-white date_Exercise'> */}
-                      <div className='container mx-auto p-2 text-3xl font-black text-white date_Exercise'>
-                      
-                        <p>{card.formattedDate.dayOfWeek}</p>
-                        <p>{card.formattedDate.day}</p>
-                        <p>{card.formattedDate.month}</p>
-
+                      <div className=' mx-auto p-2 text-3xl font-black text-white date_Exercise'> 
+      
+                        <div className='flex flex-col items-center pr-5'>
+                          <p>{card.formattedDate.dayOfWeek}</p>
+                          <p>{card.formattedDate.day}</p>
+                          <p>{card.formattedDate.month}</p>
+                        </div>
+                       
                       </div>
 
-                      <div div className='container mx-auto p-2 m-3 font-semibold text-white information text truncate ...'>
-                        <p className=''>{card.type}</p>
-                        <p>{card.name}</p>
-                        <p>{card.duration}</p>
-                  
-                        <div className='m-2 trigger'>
-                        </div>
+                      <div div className='container mx-auto p-2 m-3  text-white information text truncate ... flex flex-col items-start'>
+                        <p className='text-3xl font-semibold'>{card.type}</p>
+                        <p className='font-semibold'>{card.name}</p>
+                        <p className='font-semibold'>{card.duration}</p>
+            
                       </div>
                     </div> 
                   </Link>
@@ -106,13 +76,13 @@ function ActivityList() {
               </div>
           </div>
   
-              <div class="h-screen max-w-2xl mx-auto mt-24">
+              <div class="max-w-2xl mx-auto mt-12">
                 <div class="flex">
                   <div class="mx-auto flex gap-10">
                     <div class="w-auto h-auto">
                       <div class="flex-1 h-full">
-                        <div class="flex items-center justify-end flex-1 h-full p-2 bg-blue-800 text-white shadow rounded-full">
-                          <div class="relative">
+                        <div class="flex items-end justify-end flex-1 h-full p-2 bg-blue-800 text-white shadow rounded-full ">
+                          <div class="relative ">
                           <a href={'/activity'}>
 
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
