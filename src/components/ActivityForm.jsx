445902752844d198566fc0209f2af2ab53f9b4d7
horThana <thanawat.maphet@gmail.com>
2023-10-18 23:41:42 +0700

    edit second responsive

--- src/components/ActivityForm.jsx
+++ src/components/ActivityForm.jsx
@@ -243,7 +243,7 @@ function ActivityForm() {
             ref={(inputRef) => setFileInput(inputRef)} //Create a reference to the input element to interact with it
           />
 
-          <svg className='w-10 h-10 camera-icon' onClick={() => {fileInput.click() }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+          <svg id="camera-icon" className='w-10 h-10 camera-icon' onClick={() => {fileInput.click() }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
             <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
           </svg>
@@ -252,7 +252,7 @@ function ActivityForm() {
 
       </div>
 
-      <div className="row-2">
+      <div className="buttonzone">
         <textarea 
           placeholder="How did you feel during exercise?  ( Optional )" 
           className="text"
@@ -261,6 +261,7 @@ function ActivityForm() {
           value={formData.description}
         ></textarea>
 
+        <div className="b480">
         <div className="buttons">
           <button className='add-button'
             onClick={handleSubmit}
@@ -276,7 +277,7 @@ function ActivityForm() {
             Cancel
           </button>
         </div>
-
+        </div>
       </div>
     </div>
   )
