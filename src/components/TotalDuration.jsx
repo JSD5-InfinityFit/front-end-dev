237b559f6b40a07ebe750706729ca8531de101eb
Ruchida Pithaksiripan <im@fai.in.th>
2023-10-24 14:17:04 +0700

    add charts components for dashboard

--- src/components/TotalDuration.jsx
+++ src/components/TotalDuration.jsx
@@ -1,18 +1,15 @@
-import { React, useState, useEffect } from 'react';
-import axios from 'axios';
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 
 function Totalduration() {
   const [duration, setDuration] = useState(0);
-  const idtoken = localStorage.getItem('idtoken');
+  const idtoken = localStorage.getItem("idtoken");
   if (idtoken) {
     const decoded = jwt_decode(idtoken);
     var userID = decoded.user.userID;
   }
- 
- 
-  const BACKEND_URL = 'https://infinity-fit-backend.onrender.com';
-  const FURI = 'https://infinityfitbackenddev.onrender.com';
 
+  const BACKEND_URL = "https://infinity-fit-backend.onrender.com";
 
   const fetchDuration = async (userID) => {
     const res = await axios.get(`${BACKEND_URL}/activities/`); /// อย่าลืมเปลี่ยน URL
@@ -26,16 +23,15 @@ function Totalduration() {
     console.log(totalDuration);
   };
 
-  
   useEffect(() => {
     fetchDuration(userID);
   }, [userID]);
 
   return (
     <div>
-      <div className="card-total-duration mt-10">
+      <div className="mt-10 card-total-duration">
         <div className="shadow-lg shadow-blue-500/50 hover:scale-110  bg-sky-950 rounded-[13px] lg:w-[450px] h-[250px] md:w-[420px] m-10 ">
-          <div className="bmi-card flex flex-auto">
+          <div className="flex flex-auto bmi-card">
             <div>
               <img
                 src="https://cdn-icons-png.flaticon.com/128/11432/11432052.png"
@@ -44,10 +40,10 @@ function Totalduration() {
               />
             </div>
             <div className="total-duration">
-              <h2 className="text-2xl text-bold text-white text-center pt-10 pl-14">
+              <h2 className="pt-10 text-2xl text-center text-white text-bold pl-14">
                 Total Duration
               </h2>
-              <h2 className="text-3xl text-bold text-blue-500 text-center pt-7 pl-10">
+              <h2 className="pl-10 text-3xl text-center text-blue-500 text-bold pt-7">
                 {duration} Minutes
               </h2>
               <h3></h3>
