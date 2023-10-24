7786ae0184c3bf21f3ec669f4630aeea1361088b
horThana <thanawat.maphet@gmail.com>
2023-10-24 20:47:57 +0700

    edit resopnsive profile

--- src/components/NavbarMB.jsx
+++ src/components/NavbarMB.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import homeLogo from '../assets/icons/home.gif'
-import screenLogo from '../assets/icons/activity.gif'
-import gearLogo from '../assets/icons/setting.gif'
-import profileLogo from '../assets/icons/user.gif'
+import AnalyticsIcon from '@mui/icons-material/Analytics';
 import './Navbar.css'
+import { Grid } from '@mui/material';
 
 const NavbarMB = () => {
     return (
-        <div className="navbar">
-            <a href={'/'}><img src={homeLogo} className="icons" alt="home logo" /></a>
-            <a href={'/user/:id'}><img src={gearLogo} className="icons" alt="dashboard" /></a>
-            <a href={'/activity'}><img src={screenLogo} className="icons" alt="activity logo" /></a>
-            <a href={'/profile'}><img src={profileLogo} className="icons" alt="profile logo" /></a>
+        
+        <div>
+            <Grid href={"/"}>
+            <AnalyticsIcon/>
+            </Grid>
         </div>
+
+        // <div className="navbar">
+        //     <a href={'/'}><img src="https://cdn-icons-png.flaticon.com/128/9385/9385212.png" className="w-12 bg-red-500 rounded-3xl hover:scale-110 hover:bg-sky-200" alt="home logo" /></a>
+        //     <a href={'/user/:id'}><img src={AnalyticsIcon} className="w-12 bg-blue-500 rounded-2xl hover:scale-110 hover:bg-sky-200" alt="dashboard" /></a>
+        //     <a href={'/activity'}><img src="https://cdn-icons-png.flaticon.com/128/8888/8888175.png" className="w-12 bg-green-500 rounded-2xl hover:scale-110 hover:bg-sky-200" alt="activity logo" /></a>
+        //     <a href={'/profile'}><img src="https://cdn-icons-png.flaticon.com/128/3033/3033143.png" className="w-12 bg-indigo-500 rounded-3xl hover:scale-110 hover:bg-sky-200" alt="profile logo" /></a>
+        // </div>
     )
 }
 
--- src/components/ProfileDisplay.jsx
+++ src/components/ProfileDisplay.jsx
@@ -12,44 +12,43 @@ const ProfileDisplay = ({ setChange, information }) => {
 
   return (
    
-      <section id='card-item' className='card-items-center'>
-          <div className='pt-10 max-md:pt-10 ml-36 '>
-              <div className="information-2 w-2/5 grid gap-4 p-5 ">
-                <div className="profile-item">
-                  <span className="item-label">Email:</span>
-                  <span className="item-value">{information.userEmail}</span>
-                </div>
-                <div className="profile-item">
-                  <span className="item-label">Gender:</span>
-                  <span className="item-value">{information.userBiologicalGender}</span>
-                </div>
-                <div className="profile-item">
-                  <span className="item-label">Birthdate:</span>
-                  <span className="item-value">{information.userBD}</span>
-                </div>
-                <div className="profile-item">
-                  <span className="item-label">Height:</span>
-                  <span className="item-value">{information.userHeight}</span>
-                </div>
-                <div className="profile-item">
-                  <span className="item-label">Weight:</span>
-                  <span className="item-value">{information.userWeight}</span>
-                </div>
-              </div>
-        </div>
-
-
-          <div id="button" className="flex m-5 justify-around w-3/6 max-md:mx-20">
-        <button className="mt-4 btn-primary w-[100px]" onClick={() => setChange(false)}>
-            Edit
-          </button>
-          <button className="mt-4 btn-error w-[100px]" onClick={logout}>
-            Logout
-          </button>
+    <section id='card-item' className='card-items-center'>
+    <div className='pt-20 md:mx-60 lg:mx-96  max-md:mx-16'>
+        <div className="information-2 grid gap-4 p-5 ">
+          <div className="profile-item">
+            <span className="item-label ">Email:</span>
+            <span className="item-value">{information.userEmail}</span>
           </div>
-        
-      </section>
+          <div className="profile-item">
+            <span className="item-label">Gender:</span>
+            <span className="item-value">{information.userBiologicalGender}</span>
+          </div>
+          <div className="profile-item">
+            <span className="item-label ">Birthdate:</span>
+            <span className="item-value">{information.userBD}</span>
+          </div>
+          <div className="profile-item">
+            <span className="item-label">Height:</span>
+            <span className="item-value">{information.userHeight}</span>
+          </div>
+          <div className="profile-item">
+            <span className="item-label">Weight:</span>
+            <span className="item-value">{information.userWeight}</span>
+          </div>
+        </div>
+  </div>
 
+
+    <div id="button" className="flex justify-center mt-10 pr-10 ">
+  <button className="mt-4 btn-primary w-[100px] mx-32 max-md:mx-20" onClick={() => setChange(false)}>
+      Edit
+    </button>
+    <button className="mt-4 btn-error w-[100px] mx-32 max-md:mx-12" onClick={logout}>
+      Logout
+    </button>
+    </div>
+  
+</section>
 
 
   );
